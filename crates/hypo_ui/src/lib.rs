142260e0f877843edf387a8668f5dce0f147378e
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-09-09 10:31:08 -0400

    Bruh terrain

--- crates/hypo_ui/src/lib.rs
+++ crates/hypo_ui/src/lib.rs
@@ -1,7 +1,9 @@
 // Export
+/*
 mod root;
 mod elements;
 mod element;
 pub use root::Root;
 pub use element::Element;
 pub use elements::*;
+*/
--- crates/hypo_ui/src/root.rs
+++ crates/hypo_ui/src/root.rs
@@ -1,4 +1,30 @@
+use crate::Element;
+
 // The root UI element on the screen, contains all the elements in a binary tree fashion
 pub struct Root {
+    pub elements: Vec<Option<Element>>,
+}
 
+impl Root {
+    // Get the next free spot
+    // Add an element to the tree
+    pub fn add_element(&mut self, element: Element) {
+        // Recursively add the children onto the tree
+        let children = element.children;
+        self.elements.push(element);
+    }
+    // Remove an element from the three, and recursively remove it's children
+    pub fn remove_element(&mut self, element: Element) {
+        // Get all the children from this element, recursively 
+        let mut output_elem_indices: Vec<usize> = Vec::new();
+        let mut elems_to_evaluate: Vec<usize> = Vec::new();
+        elems_to_evaluate.extend(element.children);
+        while elems_to_evaluate.len() > 0 {
+            // We need to get the children of this element
+            let elem = self.elements.get(elems_to_evaluate[0]).unwrap();
+            let children = elem.children.clone();
+            elems_to_evaluate.extend(children);
+            elems_to_evaluate.remove(0);
+        }
+    }
 }
