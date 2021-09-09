0bfce940c5104e5041c7c5b85751e78b49c3b85d
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-09-08 20:33:54 -0400

    Gonna make the SmartList struct

--- crates/hypo_ui/src/element.rs
+++ crates/hypo_ui/src/element.rs
@@ -1,6 +1,22 @@
+use crate::Root;
+
 // A simple element, could be a button or a panel or anything, it just has some variables
 pub struct Element {
     pub size: veclib::Vector2<f32>,
     pub position: veclib::Vector2<f32>,
-    pub parent: Option<Element>,    
+    pub parent: usize,
+    id: usize,
+    pub children: Vec<usize>,    
+}
+
+impl Element {
+    // Attach children to this element
+    pub fn attach(&mut self, children: Vec<Element>) {
+        self.children = children.iter().map(|x| x.id).collect();
+    }
+    // Create a new element
+    pub fn new(root: &mut Root, position: &veclib::Vector2<f32>, size: &veclib::Vector2<f32>) {
+        // Get the element id from the root node
+        let output: Self = Element { size: size.clone(), position: position.clone(), parent: 0, id: root, children: () }
+    }
 }
