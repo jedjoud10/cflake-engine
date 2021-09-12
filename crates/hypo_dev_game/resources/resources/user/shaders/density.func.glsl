ee232dfb8e2b71e9601b05fae5fb8c32164302da
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-09-12 17:15:03 -0400

    Time to make terrain faster and implement culling

--- crates/hypo_dev_game/resources/resources/user/shaders/density.func.glsl
+++ crates/hypo_dev_game/resources/resources/user/shaders/density.func.glsl
@@ -4,10 +4,10 @@
 // Generate the density here
 float get_density(vec3 pos) {
     // Do some position flipping
-    float density = pos.y - 10.1;
-    float t = snoise(pos * 0.002) * 0.7;
-    density += snoise(pos * 0.005 * vec3(1, 0.2, 1) + vec3(t, t, t)).x * 100.0;
-    density = opSubtraction(sdSphere(pos, 100.0), density);
+    float density = pos.y + 800;
+    density -= mountain(pos.xz * 0.001, 1.0).x * 1600;
+    float sphere = max(sdSphere(pos, 100), -sdSphere(pos, 50));
+    density = max(density, -sphere);
     //density = pos.y - 10;
     return density;
 }
