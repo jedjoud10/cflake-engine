2afd4350d85ffcb6e5f4667e96a7b124b8f44668
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-09-18 13:00:10 -0400

    Gonna implement fix

--- crates/dev_game/build.rs
+++ crates/dev_game/build.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
     let target_dir = env::var("OUT_DIR").unwrap();
-    let target_dir = target_dir.split("build\\hypo_dev_game").collect::<Vec<&str>>()[0];
+    let target_dir = target_dir.split("build\\dev_game").collect::<Vec<&str>>()[0];
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     resources_packer::pack(target_dir.to_string(), project_dir);
 }
--- crates/dev_game/resources/resources/user/shaders/density.func.glsl
+++ crates/dev_game/resources/resources/user/shaders/density.func.glsl
@@ -8,6 +8,6 @@ float get_density(vec3 pos) {
     density -= mountain(pos.xz * 0.001, 1.0).x * 1600;
     float sphere = max(sdSphere(pos, 100), -sdSphere(pos, 50));
     density = max(density, -sphere);
-    density = pos.y - 10;
+    //density = pos.y - 10;
     return density;
 }
