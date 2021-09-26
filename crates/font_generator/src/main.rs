239bf072ea431177d9abaffb87d23f931a7e2dde
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-09-25 22:17:41 -0400

    We have done the goofing

--- crates/font_generator/src/main.rs
+++ crates/font_generator/src/main.rs
@@ -1,6 +1,7 @@
-use std::{env, fs::OpenOptions, io::BufWriter};
+use std::{env, fs::OpenOptions, io::{BufRead, BufReader, BufWriter}};
 
 use byteorder::{LittleEndian, WriteBytesExt};
+use fonts::FontChar;
 use image::GenericImageView;
 
 // Pack the font
@@ -13,6 +14,8 @@ fn main() {
     // Get the config file path
     let index = args.iter().position(|x| *x == "-c".to_string()).unwrap()+1;
     let config_file_path = args[index].clone();
+    let config_file = OpenOptions::new().read(true).open(config_file_path).unwrap();
+    let reader = BufReader::new(config_file);
     // Get the output file path
     let index = args.iter().position(|x| *x == "-c".to_string()).unwrap()+1;
     let output_file_path = args[index].clone();    
@@ -34,6 +37,28 @@ fn main() {
 
     // Write each pixel
     for pixel in texture.pixels() {
-        let pixel_value = pixel.2;
+        output_file_writer.write_u8(pixel.2[0]).unwrap();
+    }
+
+    // Read the config file data
+    let mut font_chars: Vec<FontChar> = Vec::new();
+    let lines = reader.lines().map(|x| x.unwrap()).collect::<Vec<String>>();
+    for line in lines {
+        // Get the ASCII character ID
+        let split_line = line.split(" ").collect::<Vec<&str>>();
+        // Check if this a char line
+        if split_line[0] == "char" {
+            // Get the ID
+            let id = split_line[1].split("id=").nth(1).unwrap().parse::<u8>().unwrap();
+            // Get the min and max
+            let x = split_line[2].split("x=").nth(1).unwrap().parse::<u32>().unwrap();
+            let y = split_line[3].split("y=").nth(1).unwrap().parse::<u32>().unwrap();
+            let width = split_line[4].split("width=").nth(1).unwrap().parse::<u32>().unwrap();
+            let height = split_line[5].split("height=").nth(1).unwrap().parse::<u32>().unwrap();
+
+            // Create the min and max from the x,y and width,height
+            let min: veclib::Vector2<f32> = min
+            println!("{} {}")
+        }
     }
 }
