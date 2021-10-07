55030b3063d4958bc41e99c7ab492ce1fe25f259
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-10-06 21:04:25 -0400

    Fixed resource packer issue, gonna make entity and material instances

--- crates/dev_game/build.rs
+++ crates/dev_game/build.rs
@@ -2,6 +2,7 @@ use std::env;
 
 // Pack the resources
 fn main() {
+    println!("cargo:rerun-if-changed=resources\\");
     let target_dir = env::var("OUT_DIR").unwrap();
     let target_dir = target_dir.split("build\\dev_game").collect::<Vec<&str>>()[0];
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
