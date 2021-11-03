2c1b934a82fe765c8852b31d60c2a56e80b763f7
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-11-02 21:30:01 -0400

    Cool voxel terrain, gonna optimize the skirts just a bit

--- crates/core/src/resources/defaults/shaders/voxel_terrain/voxel.func.glsl
+++ crates/core/src/resources/defaults/shaders/voxel_terrain/voxel.func.glsl
@@ -5,6 +5,7 @@
 #include "defaults\shaders\voxel_terrain\sdf.func.glsl"
 // Generate the voxel data here
 void get_voxel(vec3 pos, int depth, out Voxel voxel, out MaterialVoxel material_voxel) {
+    /*
     // Actual function for voxels
     int shader_id = 0;
     int texture_id = 0;
@@ -17,6 +18,28 @@ void get_voxel(vec3 pos, int depth, out Voxel voxel, out MaterialVoxel material_
     float d = min(sphere, min(box, min(rbox, p)));
     // Write the result
     voxel = Voxel(d * 300);
+    material_voxel = MaterialVoxel(shader_id, texture_id, biome_id, hardness);
+    */
+    // FBM Invertex billow noise with 8 octaves
+    float fd = 0;
+    for(int i = 0; i < 8; i++) {
+        fd += snoise(pos * vec3(1, 2.3, 1) * 0.001 * pow(2.0, i)) * 20 * pow(0.5, i);
+    }
+
+    // Add the noise
+    float density = fd * 3.0 + 0 + pos.y;
+
+    // Make the terrain flatter
+    density = min(density, pos.y);
+    density = max(density, pos.y - 30);
+    
+    int shader_id = 0;
+    int texture_id = 0;
+    int biome_id = 0;
+    int hardness = 0;
+
+    // Write the result
+    voxel = Voxel(density * 200.0);
     material_voxel = MaterialVoxel(shader_id, texture_id, biome_id, hardness);
 }
 // Generate the Vertex Color, Smoothness, Metallic and Material ID
