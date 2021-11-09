9eb0a6fd1a19584f4b948ef6e5ab6039a6f3feeb
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-11-08 21:54:52 -0500

    Indeed

--- crates/core/src/resources/defaults/shaders/others/debug.frsh.glsl
+++ crates/core/src/resources/defaults/shaders/others/debug.frsh.glsl
@@ -1,4 +1,5 @@
 #version 460 core
+#include "defaults\shaders\others\hashes.func.glsl"
 layout(location = 0) out vec3 frag_diffuse;
 layout(location = 1) out vec3 frag_normal;
 layout(location = 2) out vec3 frag_pos;
@@ -6,11 +7,14 @@ in vec3 m_position;
 in vec3 m_normal;
 in vec2 screen_position;
 uniform vec3 tint;
+uniform vec3 view_pos;
 void main() {
 	// Create a checkerboard pattern
 	float value = mod(floor(screen_position.x) + floor(screen_position.y), 2.0) == 0 ? 1.0 : 0.0;
-	frag_diffuse = tint*99.0* value;
-	// 
+	float d = abs(dot(m_normal, normalize(view_pos - m_position)));
+	if (d < (0.3 + hash12(screen_position)*0.2)) { discard; }
+	frag_diffuse = tint*2*value;
+	// Some cool blending effect
 	frag_normal = vec3(1, 1, 1);
 	frag_pos = m_position;
 }
--- crates/debug/src/renderer.rs
+++ crates/debug/src/renderer.rs
@@ -31,7 +31,6 @@ impl DebugRenderer {
         // Load the template models
         self.template_models.push(Model::asset_load_easy("defaults\\models\\cube.mdl3d", &mut asset_manager.asset_cacher).unwrap());
         self.template_models.push(Model::asset_load_easy("defaults\\models\\sphere.mdl3d", &mut asset_manager.asset_cacher).unwrap());
-        self.debug(DebugPrimitive::new().set_shape(math::shapes::Shape::new_sphere(veclib::Vector3::ZERO, 1.0)).set_tint(veclib::Vector3::ONE));
     }
     // Add a debug primitive to the queue and then render it
     pub fn debug(&mut self, debug_primitive: DebugPrimitive) {
@@ -49,7 +48,7 @@ impl DebugRenderer {
         let pos_matrix = veclib::Matrix4x4::from_translation(debug_primitive.shape.center);
         let model_matrix = match debug_primitive.shape.internal_shape {
             math::shapes::ShapeType::Cube(x) => pos_matrix * veclib::Matrix4x4::from_scale(x),
-            math::shapes::ShapeType::Sphere(x) => pos_matrix,
+            math::shapes::ShapeType::Sphere(x) => pos_matrix* veclib::Matrix4x4::from_scale(veclib::Vector3::ONE * x),
         };
         self.primitives.push(debug_primitive);
         self.renderers.push((renderer, model_matrix));
