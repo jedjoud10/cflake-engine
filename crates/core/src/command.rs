ff9310c01b28f215a3fc37e57d9b7fc3b89bb806
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-12-03 18:43:06 -0500

    MONKEY TIME

--- crates/core/src/command.rs
+++ crates/core/src/command.rs
@@ -1,16 +1,17 @@
-// Some world commands
-pub enum WorldTask {
-    // Components
-    CreateComponent(),
-    DestroyComponent(),
-    // Entity
-    CreateEntity(),
-    DestroyEntity(),
-    // Linking
-    LinkComponent(),
-    UnlinkComponent(),
+use crate::tasks::WorldTask;
+
+// A dispatch group used to dispatch multiple commands to the world at the same time, so we don't have to wait for them on the system threads
+pub struct WorldCommandDispatchGroup {
+    pub commands: Vec<WorldCommand>
 }
+// A world command
+pub struct WorldCommand {
+    pub id: u64,
+    pub task: WorldTask,
+}
+
 // A task sender that will send tasks from other thread to the main thread, asynchronously.
 pub struct WorldTaskSender {
 
 }
+
