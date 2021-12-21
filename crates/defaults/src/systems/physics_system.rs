ef04005e8b28871ab4d3df511e44620fcc5644d8
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-12-20 19:50:49 -0500

    This unsafe pointer stuff is not really safe

--- crates/defaults/src/systems/physics_system.rs
+++ crates/defaults/src/systems/physics_system.rs
@@ -1,24 +1,21 @@
 use others::callbacks::{MutCallback, NullCallback};
+use core::global::callbacks::CallbackType::LocalEntityMut;
 // Update the entities
 pub fn entity_update(data: &mut (), entity: &ecs::Entity) {
     // Update the physics
-    /*
-    let transform_global_id = core::global::ecs::component_global_id::<crate::components::Transform>(entity).unwrap();
-    let physics_global_id = core::global::ecs::component_global_id::<crate::components::Physics>(entity).unwrap();
-    core::global::ecs::entity_mut(entity, CallbackType::LocalEntityMut(MutCallback::new(move || {
+    core::global::ecs::entity_mut(entity, LocalEntityMut(MutCallback::new(|entity| {
         // Get the transform position and rotation
-        let transform = core::global::ecs::componentw_mut::<crate::components::Transform>(transform_global_id, world).unwrap();
+        let transform = core::global::ecs::component_mut::<crate::components::Transform>(entity).unwrap();
         let (position, rotation) = (&mut transform.position, &mut transform.rotation);
-        let physics = core::global::ecs::componentw_mut::<crate::components::Physics>(physics_global_id, world).unwrap();
+        let physics = core::global::ecs::component_mut::<crate::components::Physics>(entity).unwrap();
         let physics_object = &mut physics.object;
         // Apply the physics step on the position and rotation
-        physics_object.update(&mut position, &mut rotation, core::);
+        physics_object.update(&mut position, &mut rotation, core);
         let transform = components.get_component_mut::<components::Transform>(data.component_manager).unwrap();
         // Update the new position and rotation in the transform
         transform.position = position;
         transform.rotation = rotation;
-    })).create());    
-    */
+    })).create()); 
 }
 
 // Create the physics system
