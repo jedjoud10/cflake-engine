c8f069620816ace5b155691e8d4f6f2893003d1c
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-12-25 21:50:42 -0500

    Alright alright

--- crates/terrain/src/chunk_manager.rs
+++ crates/terrain/src/chunk_manager.rs
@@ -14,9 +14,6 @@ pub struct ChunkManager {
     pub chunks: HashSet<veclib::Vector3<i64>>,
     pub entities: HashMap<veclib::Vector3<i64>, usize>,
     pub entities_to_remove: HashMap<veclib::Vector3<i64>, usize>,
-    // The last frame chunk voxels where generated
-    pub last_frame_voxels_generated: u64,
-    pub voxels_generating: bool,
     // Camera location and forward vector
     pub camera_location: veclib::Vector3<f32>,
     pub camera_forward_vector: veclib::Vector3<f32>,
@@ -59,59 +56,47 @@ impl ChunkManager {
         self.camera_forward_vector = forward_vector;
     }
     // Update the chunk manager
-    pub fn update(&mut self, voxel_generator: &mut VoxelGenerator, frame_count: u64) -> Option<(Vec<(ChunkCoords, TModel)>, Vec<usize>)> {
-        // Sort the chunks to generate
-        
-        if !self.voxels_generating {
-            // Sort the added nodes using a priority system
-            let camera_position = self.camera_location;
-            let camera_forward_vector = self.camera_forward_vector;
-            self.chunks_to_generate.sort_by(|a, b| {
-                // Get the dot product
-                let ad = Self::priority_function(a, &camera_forward_vector, &camera_position);
-                let bd = Self::priority_function(b, &camera_forward_vector, &camera_position);
-                bd.partial_cmp(&ad).unwrap_or(Ordering::Equal)
-            });
-        }
+    pub fn update(&mut self, voxel_generator: &mut VoxelGenerator, frame_count: u64) -> Option<(Vec<(ChunkCoords, TModel)>, Vec<usize>)> {        
+        // Sort the added nodes using a priority system
+        let camera_position = self.camera_location;
+        let camera_forward_vector = self.camera_forward_vector;
+        self.chunks_to_generate.sort_by(|a, b| {
+            // Get the dot product
+            let ad = Self::priority_function(a, &camera_forward_vector, &camera_position);
+            let bd = Self::priority_function(b, &camera_forward_vector, &camera_position);
+            bd.partial_cmp(&ad).unwrap_or(Ordering::Equal)
+        });
         // This chunk will always have a valid model and chunk data
         let mut final_chunk: Option<TModel> = None;
-        let x = self.chunks_to_generate.get(0);
-        match x {
-            Some(coord) => {
-                // Get the chunk coords
-                let chunk_coords = coord.clone();
+        let coords = self.chunks_to_generate.remove(0);
+        
+        // Decide between generating the chunk or start the generation of the voxel data
+        if self.voxels_generating && (self.last_frame_voxels_generated + crate::FRAME_THRESHOLD) < frame_count {
+            // The voxels are generating, so wait until we reached a satisfactory frame count
+            // We reached the limit, read the compute buffer
+            self.voxels_generating = false;
+            self.last_frame_voxels_generated = 0;
 
-                // Decide between generating the chunk or start the generation of the voxel data
-                if self.voxels_generating && (self.last_frame_voxels_generated + crate::FRAME_THRESHOLD) < frame_count {
-                    // The voxels are generating, so wait until we reached a satisfactory frame count
-                    // We reached the limit, read the compute buffer
-                    self.voxels_generating = false;
-                    self.last_frame_voxels_generated = 0;
-
-                    // If we don't have a surface, no need to create a model for this chunk
-                    match voxel_generator.generate_voxels_end(chunk_coords.size, chunk_coords.depth, chunk_coords.position) {
-                        Some(voxels) => {
-                            // We have a surface, create the model
-                            // Valid TModel
-                            let tmodel = mesher::generate_model(&voxels, chunk_coords, true);
-                            final_chunk = Some(tmodel);
-                        }
-                        None => {
-                            /* We don't have a surface */                            
-                        }
-                    }
-                    self.chunks_to_generate.remove(0);
-                } else {
-                    // Uh oh
-                    if !self.voxels_generating {
-                        // The voxels didn't start generation yet, so start it
-                        voxel_generator.generate_voxels_start(chunk_coords.size, chunk_coords.depth, chunk_coords.position);
-                        self.voxels_generating = true;
-                        self.last_frame_voxels_generated = frame_count;
-                    }
+            // If we don't have a surface, no need to create a model for this chunk
+            match voxel_generator.generate_voxels_end(chunk_coords.size, chunk_coords.depth, chunk_coords.position) {
+                Some(voxels) => {
+                    // We have a surface, create the model
+                    // Valid TModel
+                    let tmodel = mesher::generate_model(&voxels, chunk_coords, true);
+                    final_chunk = Some(tmodel);
+                }
+                None => {
+                    /* We don't have a surface */                            
                 }
             }
-            None => {}
+        } else {
+            // Uh oh
+            if !self.voxels_generating {
+                // The voxels didn't start generation yet, so start it
+                voxel_generator.generate_voxels_start(chunk_coords.size, chunk_coords.depth, chunk_coords.position);
+                self.voxels_generating = true;
+                self.last_frame_voxels_generated = frame_count;
+            }
         }
         // Simply add the new generated chunk, and remove the chunks that must be removed
         let new_chunk: Vec<(ChunkCoords, TModel)> = match final_chunk {
--- crates/terrain/src/params.rs
+++ crates/terrain/src/params.rs
@@ -4,7 +4,5 @@ pub const MAIN_CHUNK_SIZE: usize = 32;
 pub const DEFAULT_LOD_FACTOR: f32 = 3.0;
 // The isoline
 pub const ISOLINE: f32 = 0.0;
-// The amount of frames that the compute shader is allowed
-pub const FRAME_THRESHOLD: u64 = 1;
 // Default terrain compute shader path
 pub const DEFAULT_TERRAIN_COMPUTE_SHADER: &str = "defaults\\shaders\\voxel_terrain\\voxel_main.cmpt.glsl";
