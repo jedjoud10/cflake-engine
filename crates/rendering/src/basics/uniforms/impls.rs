830ac560741fdb9621c6cef123c5744cede08632
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-01-11 23:24:46 -0500

    Unized matrix moment

--- crates/rendering/src/basics/uniforms/impls.rs
+++ crates/rendering/src/basics/uniforms/impls.rs
@@ -1,11 +1,14 @@
 use crate::{object::ObjectID, Texture, TextureShaderAccessType};
 
 // Some default uniforms that we will set
+#[derive(Clone)]
 pub enum Uniform {
     // These are types that are in Unsized Vectors, vectors that could be a single value, or 2, or 3, or 4
     Bool(veclib::UnsizedVector<bool>),
     I32(veclib::UnsizedVector<i32>),
     F32(veclib::UnsizedVector<f32>),
+    // Matrices
+    MAT44F32(veclib::Matr)
     // Others
     Texture(ObjectID<Texture>, u32),
     // Compute sheit
