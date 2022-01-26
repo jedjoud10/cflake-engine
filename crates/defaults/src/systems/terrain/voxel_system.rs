347f797eddad3e6bd7d0be844ac0f02641b84a36
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-01-26 16:47:51 -0500

    I guess I'll implement atomic counter while I'm at it

--- crates/defaults/src/systems/terrain/voxel_system.rs
+++ crates/defaults/src/systems/terrain/voxel_system.rs
@@ -1,10 +1,10 @@
 use main::{
     core::{Context, WriteContext},
-    ecs::{component::{ComponentQuery, ComponentID}}, terrain::{ChunkCoords, MAIN_CHUNK_SIZE}, rendering::{advanced::compute::ComputeShaderExecutionSettings, basics::{uniforms::ShaderUniformsGroup, texture::{TextureAccessType, TextureReadBytes}}, pipeline::{pipec, Pipeline}, object::PipelineTrackedTask},
+    ecs::{component::{ComponentQuery, ComponentID}, entity::EntityID}, terrain::{ChunkCoords, MAIN_CHUNK_SIZE, VoxelData}, rendering::{advanced::compute::ComputeShaderExecutionSettings, basics::{uniforms::ShaderUniformsGroup, texture::{TextureAccessType, TextureReadBytes}}, pipeline::{pipec, Pipeline}, object::PipelineTrackedTask},
 };
 
 // Start generating the voxel data for a specific chunk
-fn start_generation(terrain: &mut crate::globals::Terrain, pipeline: &Pipeline, chunk: &mut crate::components::Chunk, component_id: ComponentID) {
+fn start_generation(terrain: &mut crate::globals::Terrain, pipeline: &Pipeline, chunk: &mut crate::components::Chunk, id: EntityID) {
     // Create the compute shader execution settings and execute the compute shader
     let compute = terrain.compute_shader;
     const AXIS: u16 = (MAIN_CHUNK_SIZE + 2) as u16 / 8 + 1;
@@ -32,12 +32,150 @@ fn start_generation(terrain: &mut crate::globals::Terrain, pipeline: &Pipeline, 
     
     // Combine the tasks to make a finalizer one
     let main = pipec::tracked_finalizer(vec![execution, read_densities_tracked_id, read_materials_tracked_id], pipeline).unwrap();
-    terrain.generating = Some((main, component_id, (read_densities, read_materials)));
+    terrain.generating = Some((main, id, (read_densities, read_materials)));
     println!("Dispatched voxel generation!");
 }
 // Finish generating the voxel data and read it back, then store it into the chunk
 fn finish_generation(terrain: &mut crate::globals::Terrain, pipeline: &Pipeline, chunk: &mut crate::components::Chunk) {
+    println!("Finished voxel data generation!");
+    // Load the read containers that we passed to the pipeline
+    let (main, id, (read_densities, read_materials)) = terrain.generating.take().unwrap();
 
+    // Load the actual voxels now
+    let voxel_pixels = read_densities.fill_vec::<f32>().unwrap();
+    let material_pixels = read_materials.fill_vec::<veclib::Vector2<u8>>().unwrap();
+    
+    // Keep track of the min and max values
+    let mut min = f32::MAX;
+    let mut max = f32::MIN;
+    // Create the voxel data on the heap since it's going to be pretty big
+    let mut local_data: Box<[(f32, u8)]> = vec![(0.0, 0, 0); (MAIN_CHUNK_SIZE + 2) * (MAIN_CHUNK_SIZE + 2) * (MAIN_CHUNK_SIZE + 2)].into_boxed_slice();
+    let mut voxel_data: VoxelData = VoxelData(vec![Voxel::default(); (MAIN_CHUNK_SIZE + 1) * (MAIN_CHUNK_SIZE + 1) * (MAIN_CHUNK_SIZE + 1)].into_boxed_slice());
+
+    // 
+    for (i, density) in voxel_pixels.into_iter().enumerate() {
+        let material: veclib::Vector2<u8> = material_pixels[i];
+        // Keep the min and max
+        min = min.min(density);
+        max = max.max(density);
+        // Create the simplified voxel
+        let simplified_voxel_tuple = (density, material.x, material.y);
+        local_data[i] = simplified_voxel_tuple;
+    }
+            // If there is no surface, no need to waste time
+            let surface = min.signum() != max.signum();
+            if !surface {
+                data.results.insert(chunk_coords, Some(None));
+                /*
+                println!(
+                    "Finished voxel generation for Chunk {}, took {}ms (Async {}ms). [NO VALID SURFACE FOUND]",
+                    chunk_coords.center,
+                    i.elapsed().as_millis(),
+                    i1
+                );
+                */
+                // We finished generating data on this compute shader
+                let x = data.computes.get_mut(compute_index).unwrap();
+                x.1 = false;
+                return;
+            };
+            // Flatten using the custom size of MAIN_CHUNK_SIZE+2
+            fn custom_flatten(x: usize, y: usize, z: usize) -> usize {
+                x + (y * (MAIN_CHUNK_SIZE + 2) * (MAIN_CHUNK_SIZE + 2)) + (z * (MAIN_CHUNK_SIZE + 2))
+            }
+            // Calculate the voxel normal
+            for x in 0..(MAIN_CHUNK_SIZE + 1) {
+                for y in 0..(MAIN_CHUNK_SIZE + 1) {
+                    for z in 0..(MAIN_CHUNK_SIZE + 1) {
+                        let i = custom_flatten(x, y, z);
+                        let v0 = local_data[i];
+                        // Calculate the normal using the difference between neigboring voxels
+                        let v1 = local_data[custom_flatten(x + 1, y, z)];
+                        let v2 = local_data[custom_flatten(x, y + 1, z)];
+                        let v3 = local_data[custom_flatten(x, y, z + 1)];
+                        // Normal
+                        let normal = veclib::Vector3::new(v1.0 as f32 - v0.0 as f32, v2.0 as f32 - v0.0 as f32, v3.0 as f32 - v0.0 as f32).normalized();
+                        let sv = local_data[i];
+                        let voxel = Voxel {
+                            density: sv.0,
+                            normal,
+                            material_id: sv.1,
+                        };
+                        voxel_data.voxels[terrain::utils::flatten((x, y, z))] = voxel;
+                    }
+                }
+            }
+            /*
+            println!(
+                "Finished voxel generation for Chunk {}, took {}ms (Async {}ms)",
+                chunk_coords.center,
+                i.elapsed().as_millis(),
+                i1
+            );
+            */
+            let x = data.computes.get_mut(compute_index).unwrap();
+            x.1 = false;
+            // Tell the main system data that we finished the voxel generation for this specific chunk
+            data.results.insert(chunk_coords, Some(Some(voxel_data)));
+        }))
+        .create(),
+    );
+    */
+    // Keep track of the min and max values
+    let mut min = f32::MAX;
+    let mut max = f32::MIN;
+    // Turn the pixels into the data
+    let mut voxel_data: VoxelData = VoxelData {
+        voxels: vec![Voxel::default(); (MAIN_CHUNK_SIZE + 1) * (MAIN_CHUNK_SIZE + 1) * (MAIN_CHUNK_SIZE + 1)].into_boxed_slice(),
+    };
+    // If there is no surface, no need to waste time
+    /*
+    let surface = min.signum() != max.signum();
+    if !surface {
+        data.results.insert(chunk_coords, Some(None));
+        /*
+        println!(
+            "Finished voxel generation for Chunk {}, took {}ms (Async {}ms). [NO VALID SURFACE FOUND]",
+            chunk_coords.center,
+            i.elapsed().as_millis(),
+            i1
+        );
+        */
+        // We finished generating data on this compute shader
+        let x = data.computes.get_mut(compute_index).unwrap();
+        x.1 = false;
+        return;
+    };
+    */
+
+    // Flatten using the custom size of MAIN_CHUNK_SIZE+2
+    fn custom_flatten(x: usize, y: usize, z: usize) -> usize {
+        x + (y * (MAIN_CHUNK_SIZE + 2) * (MAIN_CHUNK_SIZE + 2)) + (z * (MAIN_CHUNK_SIZE + 2))
+    }
+    // Calculate the voxel normal
+    for x in 0..(MAIN_CHUNK_SIZE + 1) {
+        for y in 0..(MAIN_CHUNK_SIZE + 1) {
+            for z in 0..(MAIN_CHUNK_SIZE + 1) {
+                let i = custom_flatten(x, y, z);
+                // Normal
+                let voxel = Voxel {
+                    density: y as f32 - 10.0,
+                    normal: veclib::Vector3::default(),
+                    material_id: 0,
+                };
+                voxel_data.voxels[terrain::utils::flatten((x, y, z))] = voxel;
+            }
+        }
+    }
+    /*
+    println!(
+        "Finished voxel generation for Chunk {}, took {}ms (Async {}ms)",
+        chunk_coords.center,
+        i.elapsed().as_millis(),
+        i1
+    );
+    */
+    // Tell the main system data that we finished the voxel generation for this specific chunk
 }
 
 
@@ -58,16 +196,20 @@ fn run(mut context: Context, query: ComponentQuery) {
                 let mut chunk = components.component_mut::<crate::components::Chunk>().unwrap();
                 if chunk.voxel_data.is_none() {
                     // We must start generating the voxel data for this chunk
-                    start_generation(&mut *terrain, &*pipeline, &mut *chunk, components.get_id::<crate::components::Chunk>().unwrap());
+                    start_generation(&mut *terrain, &*pipeline, &mut *chunk, components.get_entity_id().unwrap());
                 }
                 None
             })
         } else {
             // We must check if we have finished generating or not
             if pipec::has_task_executed(terrain.generating.as_ref().unwrap().0, &*pipeline).unwrap() {
-                // We finished generating the voxel data for a specific chunk, and we must store it now.
-                // To find the specified chunk, we will loop through every chunk and get the one with the specified 
-                println!("Finished voxel data generation!");
+                // We will now update the chunk data to store our new voxel data
+                let id = terrain.generating.as_ref().unwrap().1;
+                query.update(id, |components| {
+                    // Get our chunk and set it's new data
+                    let mut chunk = components.component_mut::<crate::components::Chunk>().unwrap();
+                    finish_generation(&mut *terrain, &*pipeline, &mut *chunk);
+                });
             }
         }
     }
