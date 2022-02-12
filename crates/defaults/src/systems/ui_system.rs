34066b80b7b6f4f59c0dda0ac63d742ca9c61f7a
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-02-12 12:29:34 -0500

    ZAD

--- crates/defaults/src/systems/ui_system.rs
+++ crates/defaults/src/systems/ui_system.rs
@@ -7,6 +7,7 @@ pub fn system(world: &mut World) {
     world.ecs.create_system_builder().build();
     // We must create the pipeline End of Frame callback and tell it to render our UI
     let pipeline = world.pipeline.read();
+    let clone = world.ui.ui.clone();
     pipec::add_end_of_frame_callback(&pipeline, move |pipeline, _| {
         
     })
