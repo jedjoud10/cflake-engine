54633180bc5ad72f39138bc28e3d7b5ce9aea46d
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-02-26 13:33:59 -0500

    More physics shit

--- crates/physics/src/manager.rs
+++ crates/physics/src/manager.rs
@@ -1,9 +1,12 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use rapier3d::data::Arena;
 use rapier3d::math::{Rotation, Vector};
 use rapier3d::na::{Quaternion, Unit, UnitQuaternion};
 use rapier3d::prelude::{ColliderBuilder, RigidBodyBuilder, RigidBodyType, SharedShape};
-
-use crate::colliders::{Collider, ColliderType};
-use crate::identifier::{PhysicsID, PhysicsIDType};
+use crate::collider::{Collider, ColliderType};
+use crate::identifier::{PhysicsID};
 use crate::rigidbody::*;
 use crate::simulation::PhysicsSimulation;
 use crate::surface::Surface;
@@ -13,8 +16,10 @@ pub struct PhysicsManager {
     // Rapier3D simulation
     sim: PhysicsSimulation,
 
-    // Surfaces
-    surfaces: Vec<Surface>,
+    // Wrappers
+    surfaces: Arena<Surface>,
+    bodies: Arena<RigidBody>,
+    colliders: Arena<Collider>,
 }
 
 impl Default for PhysicsManager {
@@ -22,51 +27,27 @@ impl Default for PhysicsManager {
         Self {
             sim: PhysicsSimulation::new(),
             surfaces: Default::default(),
+            bodies: Default::default(),
+            colliders: Default::default(),
         }
     }
 }
 
 impl PhysicsManager {
+    // Step through the simulation
+    pub fn step(&mut self) {
+        self.sim.step();
+    }
     // Add a surface to the manager
     pub fn add_surface(&mut self, surface: Surface) -> PhysicsID<Surface> {
-        let id = self.surfaces.len();
-        self.surfaces.push(surface);
-        PhysicsID::new(PhysicsIDType::Surface(id))
+        PhysicsID::new(self.surfaces.insert(surface))
     }
     // Add a rigidbody to the manager
-    pub fn add_rigidbody(&mut self, rigidbody: RigidBody) -> PhysicsID<RigidBody> {
-        // Convert our rigidbody to a Rapier3D rigidbody
-        let _type = match rigidbody.state {
-            RigidBodyState::Static => RigidBodyType::Static,
-            RigidBodyState::Dynamic => RigidBodyType::Dynamic,
-        };
-        // Convert isometry
-        let translation = rapier3d::prelude::Translation::new(
-            rigidbody.position.x,
-            rigidbody.position.y,
-            rigidbody.position.z,
-        );
-        let rotation = UnitQuaternion::from_quaternion(Quaternion::new(
-            rigidbody.rotation[0],
-            rigidbody.rotation[1],
-            rigidbody.rotation[2],
-            rigidbody.rotation[3],
-        ));
-        // Create rigidbody
-        let rigidbody = RigidBodyBuilder::new(_type)
-            .position(rapier3d::na::Isometry {
-                rotation,
-                translation,
-            })
-            .build();
-        // And add it
-        let wrapped = self.sim.bodies.insert(rigidbody);
-
-        // Create the ID now
-        PhysicsID::new(PhysicsIDType::RigidBody(wrapped))
+    pub fn add_rigidbody(&mut self, mut rigidbody: RigidBody) -> PhysicsID<RigidBody> {
+        
     }
     // Add a shape collider to the manager
-    pub fn add_collider(&mut self, collider: Collider, rigidbody: PhysicsID<RigidBody>) -> PhysicsID<Collider> {
+    pub fn add_collider(&mut self, mut collider: Collider, rb_id: PhysicsID<RigidBody>) -> PhysicsID<Collider> {
         // Convert isometry
         let position = collider
             .shape
@@ -74,7 +55,7 @@ impl PhysicsManager {
             .unwrap_or(&veclib::Vector3::ZERO);
         let translation = rapier3d::prelude::Translation::new(position.x, position.y, position.z);
         // Create collider
-        let shared_shape = match collider.shape {
+        let shared_shape = match &collider.shape {
             ColliderType::Cuboid(cuboid) => SharedShape::cuboid(
                 cuboid.size.x / 2.0,
                 cuboid.size.y / 2.0,
@@ -84,19 +65,22 @@ impl PhysicsManager {
             ColliderType::Mesh(mesh) => panic!(),
         };
         // Get the surface parameters
-        let idx = *collider.surface.inner.as_surface().unwrap();
-        let surface: &Surface = self.surfaces.get(idx).unwrap();
+        let surface = self.surfaces.get(collider.surface.inner).unwrap();
 
         // Build the collider
-        let collider = ColliderBuilder::new(shared_shape)
+        let rapier_collider = ColliderBuilder::new(shared_shape)
             .position(rapier3d::na::Isometry {
                 rotation: Rotation::default(),
                 translation,
             })
+            .restitution(surface.restitution)
+            .friction(surface.friction)
             .build();
         // And add it
-        let wrapped = self.sim.colliders.insert_with_parent(collider, *rigidbody.inner.as_rigid_body().unwrap(), &mut self.sim.bodies);
+        let rigidbody = self.bodies.get(rb_id.inner).unwrap();
+        // Set the new handle
+        collider.handle = self.sim.colliders.insert_with_parent(rapier_collider, rigidbody.handle, &mut self.sim.bodies);
         // Create the ID now
-        PhysicsID::new(PhysicsIDType::Collider(wrapped))
+        PhysicsID::new(self.colliders.insert(collider))
     }
 }
