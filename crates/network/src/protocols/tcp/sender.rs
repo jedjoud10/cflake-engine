9c9a255980ac477ec62a15707f34170284a4fa9e
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-03-17 01:25:32 -0400

    Gonna implement the rest tomorrow

--- crates/network/src/protocols/tcp/sender.rs
+++ crates/network/src/protocols/tcp/sender.rs
@@ -1,1 +1,26 @@
 
+use std::{
+    io::{self, Error},
+    marker::PhantomData,
+    net::UdpSocket,
+};
+
+use crate::{client::Client, data::serialize_payload, protocols::EndPoint, PacketMetadata, Payload};
+
+// Packet sender
+pub struct TcpPacketSender<P: Payload + 'static> {
+    socket: UdpSocket,
+    _phantom: PhantomData<*const P>,
+    id: u64,
+}
+
+impl<P: Payload + 'static> TcpPacketSender<P> {
+    // Create a new sender using an endpoint
+    pub fn new(endpoint: &EndPoint, id: u64) -> Result<Self, Error> {
+        todo!()
+    }
+    // Send a packet to the receiver
+    pub fn send(&mut self, ) -> Result<(), Error> {
+        todo!()
+    }
+}
