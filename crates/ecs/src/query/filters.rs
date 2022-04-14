440d52e00a5c243367aea85c0392b2bb3f616057
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-04-13 22:55:32 -0400

    Going to re-implement auto registration

--- crates/ecs/src/query/filters.rs
+++ crates/ecs/src/query/filters.rs
@@ -1,1 +1,23 @@
+use std::intrinsics::transmute;
 
+use crate::{Mask, EntityState, ArchetypalState, Component};
+
+// Query filter that will block certain bundles
+pub struct QueryFilter {
+    entity: u8,
+    components: Mask,
+}
+
+impl QueryFilter {
+    // Entity state filter
+    pub fn entity(state: ArchetypalState) -> QueryFilter {
+        let state = unsafe { transmute::<ArchetypalState, u8>(state) };
+        QueryFilter { entity: state, components: Mask::zero() }
+    }
+
+    // Component state filter
+    // This will panic if the component is not registered
+    pub fn changed<T: Component>() -> QueryFilter {
+
+    }
+}
