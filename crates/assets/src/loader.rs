340fdf7eabe4f5ed0b4e76ad3de2d3d4121ba7b3
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-05-07 02:09:15 -0400

    Chadness

--- crates/assets/src/loader.rs
+++ crates/assets/src/loader.rs
@@ -8,7 +8,7 @@ use std::{
 
 // If we are in Debug, we read the bytes directly from the file system
 #[cfg(debug_assertions)]
-fn read(path: &str, asset_dir_path: PathBuf) -> Option<Vec<u8>> {
+fn read(path: &str, asset_dir_path: &PathBuf) -> Option<Vec<u8>> {
     use std::{io::Read, path::Path, fs::File};
     
     // Get the path of the file (global)
@@ -28,7 +28,7 @@ fn read(path: &str, asset_dir_path: PathBuf) -> Option<Vec<u8>> {
 
 // If we are in Release, we read the bytes from the cacher directly since they are embedded into the binary
 #[cfg(not(debug_assertions))]
-fn read(path: &str, _asset_dir_path: PathBuf) -> Option<Vec<u8>> {
+fn read(path: &str, _asset_dir_path: &PathBuf) -> Option<Vec<u8>> {
     None
 }
 
@@ -43,20 +43,32 @@ pub struct AssetLoader {
 
 impl AssetLoader {
     // Try to load an asset with some explicit optional arguments
-    pub fn load_with<A: Asset, P: AsRef<PathBuf>>(&self, path: P, args: A::OptArgs) -> T {
+    pub fn load_with<A: Asset, P: AsRef<PathBuf>>(&self, path: P, args: A::OptArgs) -> Option<A> {
         // Try to load some cached bytes, if possible
-        let meta = AssetMetadata::new(path).unwrap();
-        let cached = self.cached.get(&meta);
+        let meta = AssetMetadata::new(path.as_ref()).unwrap();
 
-        if let Some(cached) = cached {
+        // This simply deserializes the asset
+        let asset = if let Some(cached) = self.cached.get(&meta) {
             // Deserialize the asset using the cached bytes
-            let asset = A::deserialize(&meta, &cached, args);
+            A::deserialize(&meta, &cached, args)
         } else {
             // Deserialize the asset using new bytes that we will load in
-            let bytes = read(path.)
-        }
+            let path = path.as_ref().as_os_str().to_str()?;
+            let bytes = read(path, &self.global)?;
+            
+            // Deserialize first, then cache
+            let asset = A::deserialize(&meta, &bytes, args);
+            self.cached.insert(meta.clone(), bytes);
+            asset
+        };
+
+        Some(asset)
     } 
 
+    // Try to load an asset with default optional arguments
+    pub fn load<A: Asset, P: AsRef<PathBuf>>(&self, path: P) -> Option<A> where A::OptArgs: Default {
+        self.load_with(path, A::OptArgs::default())
+    }
 
     // Import an asset during compile time
     pub fn import(&mut self, path: &str, bytes: Vec<u8>) -> &[u8] {
