3b47529a8a9743f8093cb74cc0637bfba15f7d15
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-05-13 14:05:22 -0400

    I just need to fix compound assets das all

--- crates/assets/src/asset.rs
+++ crates/assets/src/asset.rs
@@ -1,5 +1,5 @@
 use std::{ffi::OsStr, path::{Path, PathBuf}, time::Instant};
-use crate::loader::{AssetLoader, Meta};
+use crate::loader::{AssetLoader, Meta, CompoundMeta};
 
 // An asset that will be loaded from a single unique file
 // Each asset has some extra data that can be used to construct the object
@@ -13,8 +13,8 @@ pub trait Asset<'args>: Sized {
     // Deserialize asset bytes, assuming that the given bytes are already in the valid format to deserialize
     fn deserialize(bytes: &[u8], args: Self::Args, meta: Meta) -> Self;
 }
-/*
-// A compound asset simply takes multiple paths to construct an object
+
+// A compound asset takes multiple different assets to construct a bigger asset
 pub trait CompoundAsset<'args>
 where
     Self: Sized,
@@ -26,6 +26,5 @@ where
     fn extensions() -> &'static [&'static str];
 
     // Given the asset loader, we shall deserialize this compound asset using a compound context loader
-    fn deserialize<'loader>(ctx: CompoundLoadingContext, args: Self::Args) -> Self;
+    fn deserialize<'loader>(tuples: &[&[u8]], args: Self::Args, meta: CompoundMeta) -> Self;
 }
-*/
--- crates/assets/src/loader.rs
+++ crates/assets/src/loader.rs
@@ -1,4 +1,4 @@
-use crate::{Asset};
+use crate::{Asset, CompoundAsset};
 use ahash::AHashMap;
 use lazy_static::lazy_static;
 use std::{
@@ -15,10 +15,10 @@ pub struct Meta {
     path: PathBuf,
 
     // The exact moment we cached the bytees
-    cached: Instant,
+    instant_bytes_cached: Instant,
 
-    // The total number of times this asset's been loaded
-    count: u64,
+    // The total number of times this asset's bytes' been access to load it or to construct a compound asset
+    num_times_accessed: u64,
 }
 
 impl Meta {
@@ -29,15 +29,28 @@ impl Meta {
 
     // Get the moment we cached the bytes
     pub fn first_cache_instant(&self) -> &Instant {
-        &self.cached
+        &self.instant_bytes_cached
     }
 
     // Get the number of times this asset's been loaded
-    pub fn count(&self) -> u64 {
-        self.count
+    pub fn accessed(&self) -> u64 {
+        self.num_times_accessed
     }
 }
 
+
+// Asset metadata for compound assets
+pub struct CompoundMeta {
+    // The paths used by each asset
+    paths: Vec<PathBuf>,
+}
+
+impl CompoundMeta {
+    // The paths used to load each asset
+    pub fn paths(&self) -> &[PathBuf] {
+        self.paths.as_slice()
+    }
+}
 
 // Asset manager that will cache all the assets and help us load them in
 pub struct AssetLoader {
@@ -59,32 +72,30 @@ impl AssetLoader {
 
     // Load an asset using some explicit loading arguments
     pub fn load_with<'loader, 'args, A: Asset<'args>>(&'loader mut self, path: &str, args: A::Args) -> Option<A> {
-        // Check if the extension is valid, and return None if it doesn't validate any of the extensions for the asset
+        // Check if the extension is valid
         let path = PathBuf::from_str(path).unwrap();
         let extension = path.extension().and_then(OsStr::to_str)?;
         (!A::extensions().contains(&extension)).then(|| ())?;
 
-        // Load the bytes from the file if they don't exist
+        // If we have no bytes currently cached, try to load and cache them
         if self.cached.get(&path).is_none() {
-            // Cache the bytes if needed (but split the path)
             let bytes = super::raw::read(&path, &self.global)?;
             self.cached.insert(path.clone(), (bytes, Instant::now(), 0));
-        }
+        };
 
-        // Create some asset metadata (and a valid byte slice) using the asset path
-        let (slice, meta) = self.cached.get_mut(&path).map(|(vec, instant, count)| {
-            let slice = vec.as_slice();
-            *count += 1;
-            
-            (slice, Meta {
-                path,
-                cached: *instant,
-                count: *count,
-            })
-        })?;
+        // Load the cached bytes and increment the accessed counter
+        let (vec, instant, count) = self.cached.get_mut(&path)?;
+        *count += 1;
 
+        // Construct meta
+        let meta = Meta {
+            path,
+            instant_bytes_cached: *instant,
+            num_times_accessed: *count,
+        };
+        
         // Deserialize the asset
-        Some(A::deserialize(slice, args, meta))
+        Some(A::deserialize(vec.as_slice(), args, meta))
     }
 
     // Load an asset using some default loading arguments
@@ -92,7 +103,44 @@ impl AssetLoader {
         self.load_with(path, Default::default())
     }
 
-    // Cache an asset manually, given it's path and it's bytes
+    // Load a compound asset using some explicit loading arguments
+    pub fn load_compound_with<'loader, 'args, A: CompoundAsset<'args>>(&'loader mut self, paths: &[&str], args: A::Args) -> Option<A> {
+        // Convert the strings into pathbufs
+        let paths = paths.iter().map(|path| PathBuf::from_str(path).ok()).collect::<Option<Vec<_>>>()?;
+        
+        // Convert all the extensions now
+        let extensions = paths.iter().map(|a| a.extension().and_then(OsStr::to_str)).collect::<Option<Vec<_>>>()?;
+
+        // Check if all the extensions are valid
+        let all_path_extensions_valid = extensions.iter().all(|ext| A::extensions().contains(ext));
+        
+        // Convert the given paths into the corresponding bytes
+        let tuples = paths.iter().map(|path| {
+            // Cache the bytes if they are invalid
+            let tuple = if self.cached.get(path).is_none() {                
+                // Load & Cache
+                let bytes = super::raw::read(&path, &self.global)?;
+                self.cached.insert(path.clone(), (bytes, Instant::now(), 0));
+                self.cached.get_mut(path)
+            } else {
+                // Load directly
+                self.cached.get_mut(path)
+            };
+
+            // Increment the accessed counter while mapping
+            tuple.map(|(vec, instant, count)| {
+                *count += 1;
+                vec.as_slice()
+            })
+        }).collect::<Option<Vec<_>>>()?;
+
+        // Create the compound metadata now, since we have all the bytes and paths
+
+        // Deserialize the asset
+        Some(A::deserialize(slice, args, meta))
+    }
+
+    // Import a persistant asset using it's global asset path and it's raw bytes
     pub fn import(&mut self, path: impl AsRef<Path>, bytes: Vec<u8>) {
         let path  = path.as_ref().strip_prefix("assets/").unwrap().to_path_buf();
         self.cached.entry(path).or_insert((bytes, Instant::now(), 0));
