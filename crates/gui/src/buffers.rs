dca7110c1f789bb0ccb04fd4f93e8265a57f1152
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-05-29 00:12:52 -0400

    Going to create a mesh vertex format thingy

--- crates/gui/src/buffers.rs
+++ crates/gui/src/buffers.rs
@@ -8,12 +8,7 @@ use rendering::object::ToGlName;
 
 // Some pre allocated buffers that we can edit everytime we draw a specific clipped mesh
 pub(crate) struct Buffers {
-    // We create a single VAO and update the buffers every time we render, sounds pretty inefficient but it works
-    pub(crate) vao: GLuint,
 
-    // Very naive dynamic buffers
-    pub(crate) indices: ElementBuffer,
-    pub(crate) vertices: ArrayBuffer<egui::epaint::Vertex>,
 }
 
 impl Buffers {
@@ -32,16 +27,13 @@ impl Buffers {
             // Bind the vertex attributes
             unsafe {
                 const STRIDE: i32 = size_of::<egui::epaint::Vertex>() as i32;
-                gl::BindBuffer(gl::ARRAY_BUFFER, vertices.name().get());
+                gl::BindBuffer(gl::ARRAY_BUFFER, vertices.name());
                 gl::EnableVertexAttribArray(0);
                 gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, STRIDE, null());
                 gl::EnableVertexAttribArray(1);
                 gl::VertexAttribPointer(1, 2, gl::FLOAT, gl::FALSE, STRIDE, (size_of::<f32>() * 2) as isize as *const c_void);
                 gl::EnableVertexAttribArray(2);
                 gl::VertexAttribPointer(2, 4, gl::UNSIGNED_BYTE, gl::FALSE, STRIDE, (size_of::<f32>() * 4) as isize as *const c_void);
-
-                // Unbind
-                gl::BindVertexArray(0);
             }
 
             // Self
@@ -49,17 +41,18 @@ impl Buffers {
             Self { vao, indices, vertices }
         }
     }
-    // Fill the buffers with new mesh data
-    pub fn fill_buffers(&mut self, vertices: Vec<egui::epaint::Vertex>, indices: Vec<u32>) {
-        //self.vertices.write(&vertices);
-        //self.indices.write(&indices);
+
+    // Get the vertex array buffer immutably
+    // Get the vertex array buffer mutably
+    // Get the element buffer immutably
+    // Get the element buffer mutably
+    pub fn ebo() -> &ElementBuffer<u32> {
+
     }
 
-    // Draw the buffers onto the screen
-    pub fn draw(&mut self) {
-        unsafe {
-            //gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, self.indices.storage().buffer());
-            //gl::DrawElements(gl::TRIANGLES, self.indices.len() as i32, gl::UNSIGNED_INT, null());
-        }
+    // Fill the buffers with new mesh data
+    pub fn fill_buffers(&mut self, ctx: &mut Context, vertices: Vec<egui::epaint::Vertex>, indices: Vec<u32>) {
+        self.vertices.write(ctx, &vertices);
+        self.indices.write(ctx, &indices);
     }
 }
