dbdac79308d4471fb9c6a4669b7929bcbe942cd7
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-06-11 16:00:26 -0400

    Added the Locked attribute to specify that some resources cannot be removed

--- crates/resources/resources-derive/src/lib.rs
+++ crates/resources/resources-derive/src/lib.rs
@@ -2,12 +2,13 @@ use proc_macro::{self, TokenStream};
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(Resource)]
+#[proc_macro_derive(Resource, attributes(Locked))]
 pub fn derive_resources(input: TokenStream) -> TokenStream {
     let DeriveInput {
-        ident, generics, ..
+        ident, generics, attrs, ..
     } = parse_macro_input!(input);
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let removable = attrs.len() == 0;
     let output = quote! {
         impl #impl_generics Resource for #ident #ty_generics #where_clause {
             fn as_any(&self) -> &dyn std::any::Any {
@@ -15,6 +16,10 @@ pub fn derive_resources(input: TokenStream) -> TokenStream {
             }
             fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
                 self
+            }
+
+            fn can_remove() -> bool {
+                #removable
             }
         }
     };
---
+++ crates/time/src/lib.rs
@@ -0,0 +1,61 @@
+use std::time::{Instant, Duration};
+
+use world::resources::Resource;
+
+// Global resource that defines the time since the start of the engine and the current frame data
+#[derive(Resource)]
+#[Locked]
+pub struct Time {
+    // The difference in seconds between the last frame and the current frame
+    delta: f32, 
+
+    // How many frames has the engine been running for?
+    frame_count: u128,
+
+    // When the engine started
+    startup: Instant,
+
+    // The start of the current frame
+    frame_start: Instant,
+}
+
+impl Default for Time {
+    fn default() -> Self {
+        Self {
+            delta: Default::default(),
+            frame_count: Default::default(),
+            startup: Instant::now(),
+            frame_start: Instant::now(),
+        }
+    }
+}
+
+impl Time {
+    // Update the time with the specified delta
+    // This should only be called by the default engine, at the start of each frame
+    pub fn update(&mut self, delta: f32) {
+        self.delta = delta;
+        self.frame_count += 1;
+        self.frame_start = Instant::now();
+    }
+
+    // Get the delta time
+    pub fn delta(&self) -> f32 {
+        self.delta
+    }
+
+    // Get the frame count
+    pub fn frame_count(&self) -> u128 {
+        self.frame_count
+    }
+
+    // Get the moment we started the engine
+    pub fn startup(&self) -> Instant {
+        self.startup
+    }
+
+    // Get the moment the current frame started
+    pub fn frame_start(&self) -> Instant {
+        self.frame_start
+    }
+}
