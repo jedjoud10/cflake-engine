d2c7682b90b222adbef911d1d228ddc76ab0ab10
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-06-24 12:45:54 -0400

    Alright I found a solution to this event problemo, currently implementing it. Getting back on the grind yk

--- crates/world/src/events.rs
+++ crates/world/src/events.rs
@@ -1,91 +1,80 @@
-use std::{any::{TypeId, Any}, rc::Rc, cell::{RefCell, RefMut}};
+use std::{rc::Rc, cell::RefCell, any::{TypeId, Any}};
+
 use ahash::AHashMap;
-use glutin::event::WindowEvent;
+
 use crate::World;
-
-pub trait Event<'a, M: MarkerDescriptor<'a>>: 'static {
-    fn call(&self, data: M::Inner);
-}
-
-pub trait MarkerDescriptor<'a>: 'static {
-    type Inner: 'a;
-}
 
 struct Init;
 struct Update;
 
-
-impl<'a> MarkerDescriptor<'a> for Init {
-    type Inner = &'a mut World;
+pub trait Descriptor<'a> {
+    type Params: 'a;
 }
 
-impl<'a> MarkerDescriptor<'a> for Update {
-    type Inner = &'a mut World;
+impl<'a> Descriptor<'a> for Init {
+    type Params = &'a mut World;
 }
 
+impl<'a> Descriptor<'a> for Update {
+    type Params = &'a mut World;
+}
 
-impl<'a, F> Event<'a, Init> for F where F: Fn(&mut World) + 'static {
-    fn call(&self, data: <Init as MarkerDescriptor>::Inner) {
-        self(data);
+pub trait Event<Params> {
+    fn call(&self, params: &mut Params);
+}
+
+impl<F> Event<&mut World> for F where F: Fn(&mut World) + 'static {
+    fn call(&self, params: &mut &mut World) {
+        self(params)
     }
 }
 
-impl<'a, F> Event<'a, Update> for F where F: Fn(&mut World) + 'static {
-    fn call(&self, data: <Update as MarkerDescriptor>::Inner) {
-        self(data);
-    }
-}
+// These are specialized events that can be executed with any parameter type
+struct SpecializedEvents<Params>(Vec<(Box<dyn Event<Params>>, i32)>, i32);
 
-// This trait will be implemented for vectors that contain multiple closures of a specific type
-// This should allow for less heap allocation in total, since most closures will just be function pointers (fn())
-trait EventStorage {
-    fn as_any(&self) -> &dyn Any;
-    fn as_any_mut(&mut self) -> &mut dyn Any;
-    fn sort(&mut self);
-}
-
-// Implementations of event storage that's kinda wrong but it works
-impl<T: Sized + 'static> EventStorage for Vec<(T, i32)> {
-    fn as_any(&self) -> &dyn Any {
-        self
+impl<P> SpecializedEvents<P> {
+    // Register a new specialized event with a specific priority index
+    fn register_with(&mut self, event: impl Event<P> + 'static, priority: i32) {
+        self.0.push((Box::new(event), priority));
     }
 
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
+    // Register a new specialized event with an automatic priority index
+    fn register(&mut self, event: impl Event<P> + 'static) {
+        self.0.push((Box::new(event), self.1));
+        self.1 += 1;
     }
 
+    // Call each boxed event with the appropriate given parameters
+    fn call(&mut self, mut params: P) {
+        for (event, _) in self.0.iter_mut() {
+            event.call(&mut params);
+        }
+    }
+
+    // Sort the specialized events based on their priority index
     fn sort(&mut self) {
-        self.sort_by(|(_, a), (_, b)| i32::cmp(a, b));
+        self.0.sort_by(|(_, a), (_, b)| i32::cmp(a, b));
     }
 }
 
-// Normal event map and the shared map (interior mutability)
-// These event maps contain the events in this hierarchy
-// TypeId -> Marker Storager -> Vector Storage -> Closures 
-type EventMap = AHashMap<TypeId, (Box<dyn EventStorage>, i32)>;
-type SharedEventMap = Rc<RefCell<EventMap>>;
+// This event map will contain multiple boxed SpecializedEvents<P>
+type EventMap = AHashMap<TypeId, Box<dyn Any>>;
 
-// This is the main event handler that will be cloned (shared)
-// We can clone this safely without paining ourselves since the inner data can be simply just cloned and extracted when needed
-// This stores each closure as it's own boxed type, so if we have a bunch of closures that implement the same 
+// Shared map for interior mutability. This also allows us to clone the main events anywhere we want
+type SharedMap = Rc<RefCell<EventMap>>;
+
+// These are the global events interface that we will be accessing. This allows us to register, sort, and execute specific events given their descriptor
 #[derive(Default)]
-pub struct Events(SharedEventMap);
+pub struct Events(SharedMap);
 
 impl Events {
-    // Register a new event with an automatic priority index
-    pub fn register<'a, M: MarkerDescriptor<'a>>(&self, event: impl Event<'a, M>) {
+    // Register a new event using it's marker descriptor and it's priority index
+    pub fn register_with<'a, D: Descriptor<'a>>(&self, event: impl Event<D::Params> + 'static, priority: i32) {
+
     }
 
-    // Register a new event with a given priority index
-    pub fn register_with<'a, M: MarkerDescriptor<'a>>(&self, event: impl Event<'a, M>, priority: i32) {
+    // Register a new event using it's marker descriptor and an automatic priority index
+    pub fn register<'a, D: Descriptor<'a>>(&self, event: impl Event<D::Params> + 'static) {
+        
     }
-
-    // Sort the events that are created using a specific marker type
-    pub fn sort<'a, M: MarkerDescriptor<'a>>(&self) {
-    }
-
-    // Execute all the boxed events of a specific type 
-    pub fn execute<'a, M: MarkerDescriptor<'a>>(&self, data: M::Inner) {
-
-    } 
 }
