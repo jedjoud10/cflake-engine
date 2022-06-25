f52d36733e78f7058abd223e1cfebada5ecc039b
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-06-25 12:22:51 -0400

    Finally fixed the goddamn event code. Took me a whole fucking week to fix holy shit

--- crates/world/src/events.rs
+++ crates/world/src/events.rs
@@ -7,70 +7,55 @@ use crate::World;
 struct Init;
 struct Update;
 
-pub trait Descriptor: 'static {
-    type Params: 'static + ArgsTuple;
+pub trait Descriptor<'a>: Sized + 'static {
+    type Params: 'a;
 }
 
-impl Descriptor for Init {
-    type Params = EvWrite<World>;
+pub trait Event<'a, F>: Descriptor<'a> {
+    fn execute(func: &F, params: &mut Self::Params);
 }
 
-impl Descriptor for Update {
-    type Params = EvWrite<World>;
+pub trait BoxedEvent<Marker> {
+    fn execute<'a>(&self, params: &mut <Marker as Descriptor<'a>>::Params) where Marker: Descriptor<'a>;
 }
 
-pub trait Event<Params> {
-    fn call<'a>(&self, params: &mut <Params as ArgsTupleConverter<'a>>::Inner) where Params: ArgsTupleConverter<'a>;
+impl<'a> Descriptor<'a> for Init {
+    type Params = &'a mut World;
 }
 
-impl<F> Event<EvWrite<World>> for F where F: Fn(&mut World) + 'static {
-    fn call<'a>(&self, params: &mut <EvWrite<World> as ArgsTupleConverter<'a>>::Inner) where EvWrite<World>: ArgsTupleConverter<'a> {
-        todo!()
+impl<'a, F: Fn(&mut World) + 'static> Event<'a, F> for Init {
+    fn execute(func: &F, params: &mut Self::Params) {
+        let world: &mut World = params;
+        func(world);
     }
 }
 
-pub trait ArgsTuple {
+impl<F> BoxedEvent<Init> for F where F: Fn(&mut World) + 'static {
+    fn execute<'a>(&self, params: &mut <Init as Descriptor<'a>>::Params) where Init: Descriptor<'a> {
+        <Init as Event<'a, F>>::execute(self, params)
+    }
 }
-pub trait ArgsTupleConverter<'a> {
-    type Inner: 'a;
-}
-trait Handler {
-}
-pub trait HandlerConverter<'a> {
-    type Inner: 'a;
-}
-struct EvWrite<T>(PhantomData<*mut T>);
-struct EvRead<T>(PhantomData<*const T>);
-impl<T: 'static> Handler for EvWrite<T> {}
-impl<T: 'static> Handler for EvRead<T> {}
-impl<T: Handler> ArgsTuple for T {}
-impl<A: Handler, B: Handler> ArgsTuple for (A, B) {}
-impl<'a, T: HandlerConverter<'a>> ArgsTupleConverter<'a> for T {
-    type Inner = T::Inner;
-}
-impl<'a, A: HandlerConverter<'a>, B: HandlerConverter<'a>> ArgsTupleConverter<'a> for (A, B) {
-    type Inner = (A::Inner, B::Inner);
-}
+
 
 // These are specialized events that can be executed with any parameter type
-struct SpecializedEvents<Params: 'static + ArgsTuple>(Vec<(Box<dyn Event<Params>>, i32)>, i32);
+struct SpecializedEvents<Marker: 'static>(Vec<(Box<dyn BoxedEvent<Marker>>, i32)>, i32);
 
-impl<P: 'static + ArgsTuple> SpecializedEvents<P> {
+impl<Marker: 'static> SpecializedEvents<Marker> {
     // Register a new specialized event with a specific priority index
-    fn register_with(&mut self, event: impl Event<P> + 'static, priority: i32) {
+    fn register_with(&mut self, event: impl BoxedEvent<Marker> + 'static, priority: i32) {
         self.0.push((Box::new(event), priority));
     }
 
     // Register a new specialized event with an automatic priority index
-    fn register(&mut self, event: impl Event<P> + 'static) {
+    fn register(&mut self, event: impl BoxedEvent<Marker> + 'static) {
         self.0.push((Box::new(event), self.1));
         self.1 += 1;
     }
 
-    // Call each boxed event with the appropriate given parameters
-    fn call<'a>(&mut self, mut params: P::Inner) where P: ArgsTupleConverter<'a> {
-        for (event, _) in self.0.iter_mut() {
-            event.call(&mut params);
+    // Execute all the events that are stored within this set
+    fn execute<'a>(&self, mut params: Marker::Params) where Marker: Descriptor<'a> {
+        for (event, _) in self.0.iter() {
+            event.execute(&mut params);
         }
     }
 
@@ -80,7 +65,7 @@ impl<P: 'static + ArgsTuple> SpecializedEvents<P> {
     }
 }
 
-// This event map will contain multiple boxed SpecializedEvents<P>
+// This event map will contain multiple boxed SpecializedEvents<Marker>
 type EventMap = AHashMap<TypeId, Box<dyn Any>>;
 
 // Shared map for interior mutability. This also allows us to clone the main events anywhere we want
@@ -92,28 +77,31 @@ pub struct Events(SharedMap);
 
 impl Events {
     // This will get an interior reference to a specialized event set that uses the parameters from a specific descriptor
-    fn fetch<D: Descriptor>(&self) -> RefMut<SpecializedEvents<D::Params>> {
+    fn fetch<'a, Marker: Descriptor<'a>>(&self) -> RefMut<SpecializedEvents<Marker>> {
         RefMut::map(self.0.borrow_mut(), |hashmap| {
-            let boxed = hashmap.entry(TypeId::of::<D>()).or_insert_with(|| Box::new(SpecializedEvents::<D::Params>(Vec::default(), 0)));
-            let specialized = boxed.downcast_mut::<SpecializedEvents<D::Params>>().unwrap();
+            let boxed = hashmap.entry(TypeId::of::<Marker>()).or_insert_with(|| Box::new(SpecializedEvents::<Marker>(Vec::default(), 0)));
+            let specialized = boxed.downcast_mut::<SpecializedEvents<Marker>>().unwrap();
             specialized            
         })
     }
 
     // Register a new event using it's marker descriptor and it's priority index
-    pub fn register_with<D: Descriptor>(&self, event: impl Event<D::Params> + 'static, priority: i32) {
-        self.fetch::<D>().register_with(event, priority);
+    pub fn register_with<'a, Marker: Descriptor<'a>>(&self, event: impl BoxedEvent<Marker> + 'static, priority: i32) {
+        self.fetch::<Marker>().register_with(event, priority);
     }
 
     // Register a new event using it's marker descriptor and an automatic priority index
-    pub fn register<D: Descriptor>(&self, event: impl Event<D::Params> + 'static) {
-        self.fetch::<D>().register(event);
+    pub fn register<'a, Marker: Descriptor<'a>>(&self, event: impl BoxedEvent<Marker> + 'static) {
+        self.fetch::<Marker>().register(event);
+    }
+
+    // Sort the events based on their priority for a specific marker descriptor type
+    pub fn sort<'a, Marker: Descriptor<'a>>(&self) {
+        self.fetch::<Marker>().sort();
     }
 
     // Execute all the events using a specific marker descriptor type
-    // This will return the number of events that were successfully executed
-    pub fn execute<'a, D: Descriptor>(&self, params: <D::Params as ArgsTupleConverter<'a>>::Inner) -> Option<usize> where D::Params: ArgsTupleConverter<'a> {
-        self.fetch::<D>().call(params);
-        None
+    pub fn execute<'a, Marker: Descriptor<'a>>(&self, params: Marker::Params) {
+        self.fetch::<Marker>().execute(params);
     }
 }
