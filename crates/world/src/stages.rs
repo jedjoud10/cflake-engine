4b8b4cd469c1c9b47387c599054f5fcf2ea0b608
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-06-29 16:00:30 -0400

    sad

--- crates/world/src/stages.rs
+++ crates/world/src/stages.rs
@@ -5,8 +5,8 @@ use ahash::{AHashMap, AHashSet};
 // Name key type
 type Key = &'static str;
 
-// A restriction is some sort of rule that depicts the arrangement and the location of the stages
-enum Restriction {
+// A rule that depicts the arrangement and the location of the stages relative to other stages
+enum Rule {
     Before(Key),
     After(Key)
 }
@@ -16,79 +16,97 @@ pub struct Stage {
     // The user defined name of the current stage
     name: Key,
 
-    // Restrictions that apply to this stage
-    restrictions: Vec<Restriction>,
+    // Rules that apply to this stage
+    rules: Vec<Rule>,
+
+    // Current stage group. Becomes None if we have any rules
+    level: Option<usize>,
 }
 
 impl Stage {
-    // Create a stage that will execute anytime, since it has no restrictions
+    // Create a stage that will execute anytime, since it has no rules
     pub fn new(name: impl Into<Key>) -> Self {
-        Self { name: name.into(), restrictions: Default::default() }
+        Self { name: name.into(), rules: Default::default(), level: Some(0) }
     }
 
-    // Add a restriction to this stage hinting it that it should execute before other
+    // Add a rule to this stage hinting it that it should execute before other
     pub fn before(mut self, other: impl Into<Key>) -> Self {
-        self.restrictions.push(Restriction::Before(other.into()));
+        self.rules.push(Rule::Before(other.into()));
+        self.level = None;
         self
     }
 
-    // Add a restriction to this stage hinting it that it should execute after other
+    // Add a rule to this stage hinting it that it should execute after other
     pub fn after(mut self, other: impl Into<Key>) -> Self {
-        self.restrictions.push(Restriction::After(other.into()));
+        self.rules.push(Rule::After(other.into()));
+        self.level = None;
         self
     }
 
-    // Check if the stage references the "other" stage in any way 
-    fn is_parent(&self, other: Key) -> bool {
-        self.restrictions.iter().any(|r| match *r {
-            Restriction::Before(k) => k == other,
-            Restriction::After(k) => k == other,
-        })
+    // Free nodes are nodes that don't have any rules associated to them
+    pub fn free(&self) -> bool {
+        self.rules.is_empty()
     }
 }
 
-// Calculate all the priority indices from a set of stages
-pub fn evaluate(map: AHashMap<Key, Stage>) {
-    // This contains the priorities associated with each stage
-    let mut priorities: AHashMap<Key, i32> = AHashMap::new();
 
-    // These are the stages that we must evalute for currently
-    let mut eval: AHashSet<Key> = AHashSet::new();
+// Calculate all the priority indices of the stages and sort them automatically
+fn evaluate(vec: Vec<Stage>) -> Vec<Stage> {
+    // Make sure there are no duplicates, and index by name 
+    let map: AHashMap<Key, Stage> = AHashMap::from_iter(vec.into_iter().map(|stage| (stage.name, stage)));
 
-    // Evaluate the stages that have no restriction (p = 0)
-    for (_, stage) in map.iter().filter(|(_, stage)| stage.restrictions.is_empty()) {
-        eval.insert(stage.name);
+    // This is the list of nodes that we must evaluate
+    let mut eval: Vec<Key> = Vec::new();
+
+    // Evaluate the nodes that don't have any rules first
+    for node in map.values().filter(|s| Stage::free(s)) {
+        eval.push(node.name);
     }
 
-    // Repeatedly evalute the stages
-    loop {
-        for key in eval.drain() {
-            // Calculate the priority index for the current node
-            let stage = &map[key];
-            if stage.restrictions.is_empty() {
-                // If we have no restrictions, the priority index is 0
-                priorities.insert(key, 0);
-            } else {
-                // Calculate the priority index based on restrictions
-                for restriction in stage.restrictions.iter() {
-                    let parent = match restriction {
-                        Restriction::Before(parent) => &map[parent],
-                        Restriction::After(parent) => &map[parent],
-                    }
-                }  
-            }
+    // A multidimensional registry to contain all the levels and nodes
+    let mut levels: Vec<Vec<Key>> = Vec::new();
+
+    // Push a new key into the levels
+    fn insert_key(key: Key, idx: usize, levels: &mut Vec<Vec<Key>>) {
+        if levels.len() < (idx + 1) {
+            levels.push(Vec::new());
+        } else {
+            let level = &mut levels[idx];
+            level.push(key);
         }
     }
 
-    Stage::new(rendering).after("input").before("internal")
+    // Recursively evaluate the nodes
+    loop {
+        for name in eval.drain(..) {
+            // Try to insert the node into the proper levels
+            let node = &map[name];
 
-    // Evalue the stages that use our current evaluted nodes as parents (down a level in tree)
-    for stage in map.values().filter(|stage| stage.is_parent( ))
-}
+            // Check if the node has rules
+            if node.free() {
+                insert_key(node.name, 0, &mut levels);
 
-#[test]
-fn test() {
-    let main = Stage::new("main");
-    let after = Stage::new("after").after("main");
-    let before = Stage::new("before").before("main");
+                // We must now evaluate the nodes that use the current node, aka the children
+            } else {
+                //insert_key(node.name, )
+
+                // We have multiple rules, so we must restrict our node
+                for rule in node.rules.iter() {
+                    match rule {
+                        Rule::Before(other) => {
+                            // Check if the current node position is on the same level as other
+                            //    if it is, then make a new level before the leve of other and swap
+                            // Move the node before other                            
+                        },
+                        Rule::After(other) => {
+
+                        },
+                    }
+                }
+            }
+
+        }
+    }
+
+    Vec::new()
 }
