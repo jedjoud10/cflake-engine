1f65a0c77b0af7e656983a70d765ddf5b0cd4e87
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-07-17 14:26:24 -0400

    Removing ECS state for simplicity

--- crates/ecs/src/entity/entry.rs
+++ crates/ecs/src/entity/entry.rs
@@ -1,79 +1,146 @@
 use std::marker::PhantomData;
 
 use super::Entity;
-use crate::{registry, Archetype, Component, EcsManager, EntryError, Mask, QueryLayout};
+use crate::{registry::{self, mask, name}, Archetype, Component, EcsManager, EntryError, Mask, QueryLayout, ArchetypeSet, EntitySet, StateRow, EntityLinkings};
 
 // An entity entry that we can use to access multiple components on a single entity
 // This will take an immutable reference of the ECS manager, so we cannot mutate any of the underlying entity components
 pub struct Entry<'a> {
     archetype: &'a Archetype,
-    bundle: usize,
+    linkings: EntityLinkings,
     _phantom: PhantomData<&'a EcsManager>,
 }
 
 impl<'a> Entry<'a> {
-    /*
-    // Create an entry from the Ecs manager and an entity
-    pub(crate) fn new(manager: &'a EcsManager, entity: Entity) -> Option<Self> {
+    // Create an immutable entity entry from the ecs manager and an entity
+    pub fn new(manager: &'a EcsManager, entity: Entity) -> Option<Self> {
         let linkings = manager.entities.get(entity)?;
         Some(Self {
             archetype: manager.archetypes.get(&linkings.mask)?,
-            bundle: linkings.bundle,
+            linkings,
             _phantom: Default::default(),
         })
     }
 
-    // Try to get a component mask
-    fn mask<T: Component>(&self) -> Result<Mask, EntryError> {
-        let mask = registry::mask::<T>();
-        if self.archetype.mask() & mask != mask {
-            return Err(EntryError::MissingComponent(registry::name::<T>()));
-        }
-        Ok(mask)
+    // Get the entity linkings of the current entity
+    pub fn linkings(&self) -> EntityLinkings {
+        self.linkings
+    }
+
+    // Get an immutable reference to the enitity's archetype
+    pub fn archetype(&self) -> &Archetype {
+        &self.archetype
+    }
+    
+    // Get a mutable reference to the enitity's archetype
+    pub fn archetype_mut(&mut self) -> &mut Archetype {
+        &mut self.archetype
+    }
+
+    // Get an immutable reference to a table
+    pub fn table<T: Component>(&self) -> Result<&Vec<T>, EntryError> {
+        self.archetype().table::<T>().ok_or(EntryError::MissingComponent(name::<T>()))
     }
 
     // Get an immutable reference to a linked component
     pub fn get<T: Component>(&self) -> Result<&T, EntryError> {
-        let mask = self.mask::<T>()?;
-        let boxed = self.archetype.storage().get(&mask).unwrap();
-        let vec = boxed.as_any().downcast_ref::<Vec<T>>().unwrap();
-        Ok(&vec[self.bundle])
+        self.table::<T>().map(|vec| &vec[self.linkings.index()])
+    }
+    
+    // Get the current state row of our entity
+    pub fn states(&self) -> StateRow {
+        self.archetype().states().get(self.linkings.index()).unwrap()
     }
 
-    // Check if a specific component was mutated
-    pub fn was_mutated<T: Component>(&self) -> Result<bool, EntryError> {
-        let mask = self.mask::<T>()?;
-        let mutated = self.archetype.states().get(self.bundle).unwrap();
-        Ok(mutated.mutated(mask.offset()))
+    // Check if the entity has a component linked to it
+    pub fn contains<T: Component>(&self) -> bool {
+        self.archetype().mask().contains(mask::<T>())
     }
-    */
 }
 
 // An entity entry that we can use to access multiple components on a single entity
 // This will take a mutable reference of the ECS manager. Use Entry instead if you wish for an immutable entry
 pub struct EntryMut<'a> {
-    archetype: &'a mut Archetype,
-    bundle: usize,
+    archetypes: &'a mut ArchetypeSet,
+    entities: &'a mut EntitySet,
+    entity: Entity,
+    linkings: EntityLinkings,
     _phantom: PhantomData<&'a mut EcsManager>,
 }
 
 impl<'a> EntryMut<'a> {
-    /*
     // Create a mutable entry from the ecs manager and an entity
-    pub(crate) fn new(manager: &'a mut EcsManager, entity: Entity) -> Option<Self> {
-        let linkings = manager.entities.get(entity)?;
+    pub fn new(manager: &'a mut EcsManager, entity: Entity) -> Option<Self> {
         Some(Self {
-            archetype: manager.archetypes.get_mut(&linkings.mask)?,
-            bundle: linkings.bundle,
+            archetypes: &mut manager.archetypes,
+            entities: &mut manager.entities,
+            entity,
+            linkings: manager.entities.get(entity)?,
             _phantom: Default::default(),
         })
+    }
+    
+    // Get the entity linkings of the current entity
+    pub fn linkings(&self) -> EntityLinkings {
+        self.linkings
+    }
+
+    // Get an immutable reference to the enitity's archetype
+    pub fn archetype(&self) -> &Archetype {
+        self.archetypes.get(&self.linkings().mask()).unwrap()
+    }
+    
+    // Get a mutable reference to the enitity's archetype
+    pub fn archetype_mut(&mut self) -> &mut Archetype {
+        self.archetypes.get_mut(&self.linkings().mask()).unwrap()
+    }
+
+    // Get an immutable reference to a table
+    pub fn table<T: Component>(&self) -> Result<&Vec<T>, EntryError> {
+        self.archetype().table::<T>().ok_or(EntryError::MissingComponent(name::<T>()))
+    }
+
+    // Get a mutable reference to a table
+    pub fn table_mut<T: Component>(&mut self) -> Result<&mut Vec<T>, EntryError> {
+        self.archetype_mut().table_mut::<T>().ok_or(EntryError::MissingComponent(name::<T>()))
+    }
+
+    // Get an immutable reference to a linked component
+    pub fn get<T: Component>(&self) -> Result<&T, EntryError> {
+        self.table::<T>().map(|vec| &vec[self.linkings().index()])
+    }
+
+    // Get a mutable reference to a linked component
+    pub fn get_mut<T: Component>(&mut self) -> Result<&mut T, EntryError> {
+        let states = self.archetype_mut().states();
+        states.update()
+        Ok(&mut table[self.linkings().index()])
+    }
+
+    // Get a mutable reference to a linked component, but without triggering a StateRow mutation change
+    pub fn get_mut_silent(&mut self) -> Result<&mut T, EntryError> {
+        self.table_mut::<T>().map(|vec| &mut vec[self.index])
+    }
+
+
+    // Add a new component bundle to the entity, forcing it to switch archetypes
+    // Remove an old component bundle from the entity, forcing it to switch archetypes
+    
+    // Get the current state row of our entity
+    pub fn states(&self) -> StateRow {
+        self.archetype.states().get(self.index).unwrap()
+    }
+
+    // Check if the entity has a component linked to it
+    pub fn contains<T: Component>(&self) -> bool {
+        self.archetype.mask().contains(mask::<T>())
     }
 
     // Try to get a component mask
     fn mask<T: Component>(&self) -> Result<Mask, EntryError> {
-        let mask = registry::mask::<T>();
+        let mask = mask::<T>();
         if self.archetype.mask() & mask != mask {
-            return Err(EntryError::MissingComponent(registry::name::<T>()));
+            return Err(EntryError::MissingComponent(name::<T>()));
         }
         Ok(mask)
     }
@@ -89,18 +156,8 @@ impl<'a> EntryMut<'a> {
     // Get a mutable reference to a linked component
     pub fn get_mut<T: Component>(&mut self) -> Result<&mut T, EntryError> {
         let mask = self.mask::<T>()?;
-        self.archetype
-            .states()
-            .update(self.bundle, |mutated, _| mutated.set(mask.offset(), true));
-        self.get_mut_silent()
-    }
 
-    // Get a mutable reference to a linked component silently, without triggering a mutation state change
-    pub fn get_mut_silent<T: Component>(&mut self) -> Result<&mut T, EntryError> {
-        let mask = self.mask::<T>()?;
-        let boxed = self.archetype.storage_mut().get_mut(&mask).unwrap();
-        let vec = boxed.as_any_mut().downcast_mut::<Vec<T>>().unwrap();
-        Ok(&mut vec[self.bundle])
+        self.get_mut_silent()
     }
 
     // Check if a specific component was mutated
