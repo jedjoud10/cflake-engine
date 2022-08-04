2eadff9bb080bcff3f5a5e0c0655a7ffe68cba42
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2022-08-04 19:18:06 +0200

    mathematical equation without the sus

--- crates/math/src/octrees.rs
+++ crates/math/src/octrees.rs
@@ -1,8 +1,8 @@
-mod diff_octree;
+mod differential;
 mod settings;
 mod node;
 mod octree;
-pub use diff_octree::*;
+pub use differential::*;
 pub use settings::*;
 pub use node::*;
 pub use octree::*;
