20b5923785c9c409adf1b005c4c91d0746790058
jedjoud10 <jribijed6@gmail.com>
2022-11-10 09:35:30 -0500

    Writing the initialization Vulkan code

---
+++ crates/rendering/src/context/graphics.rs
@@ -0,0 +1,2 @@
+// Graphical context that we will wrap around the Vulkan instance
+// This will also wrap the logical device that we will select
---
+++ crates/rendering/src/context/window.rs
@@ -0,0 +1,4 @@
+// A window is what we will draw to at the end of each frame
+pub struct Window {
+
+}
