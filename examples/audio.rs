f0e5d43cc23d392423d909e3661b7f8a8bbd889f
jedjoud10 <jribijed6@gmail.com>
2022-12-08 23:06:35 -0500

    Le procedural sound effects

--- examples/audio.rs
+++ examples/audio.rs
@@ -14,7 +14,7 @@ fn init(world: &mut World) {
     // Get the required resources
     let mut assets = world.get_mut::<Assets>().unwrap();
     let mut scene = world.get_mut::<Scene>().unwrap();
-    scene.insert(AudioListener::new().unwrap());
+    scene.insert(AudioPlayer::new().unwrap());
 
     // Register the assets
     asset!(&mut assets, "assets/user/ignored/nicolas.mp3");
@@ -26,9 +26,10 @@ fn init(world: &mut World) {
 
     // Create audio sources for the clips
     let mut source1 = AudioSource::new(clip1);
-    source1.set_volume(0.0f32);
+    source1.filte
+    //source1.set_volume(0.0f32);
     let mut source2 = AudioSource::new(clip2);
-    source2.set_volume(0.6f32);
+    //source2.set_volume(0.6f32);
 
     // Insert both audio sources
     scene.extend_from_iter([source1, source2]);
