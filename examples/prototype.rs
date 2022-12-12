a231c022f834994ceab5ead40f7430d5249b9ff4
jedjoud10 <jribijed6@gmail.com>
2022-12-12 11:44:42 -0500

    Gotta handle automatic pipeline creation somehow

--- examples/prototype.rs
+++ examples/prototype.rs
@@ -6,6 +6,7 @@ fn main() {
         .set_window_title("cflake engine prototype example")
         .insert_update(update)
         .insert_init(init)
+        .set_frame_rate_limit(FrameRateLimit::VSync)
         .execute();
 }
 
@@ -13,8 +14,36 @@ fn main() {
 fn init(world: &mut World) {
     let graphics = world.get::<Graphics>().unwrap().clone();
     let mut threadpool = world.get_mut::<ThreadPool>().unwrap();
-    let recorder = graphics.acquire();
-    graphics.submit(recorder);
+    let mut recorder = graphics.acquire();
+
+    unsafe {
+        let device = graphics.device();
+        let queue = graphics.queue();
+        let usage = cflake_engine::graphics::vk::BufferUsageFlags::TRANSFER_SRC | 
+        cflake_engine::graphics::vk::BufferUsageFlags::TRANSFER_DST;
+        let copy = *cflake_engine::graphics::vk::BufferCopy::builder()
+            .src_offset(0)
+            .dst_offset(0)
+            .size(4);
+        let loc = cflake_engine::graphics::MemoryLocation::CpuToGpu;
+        let (buffer, allocation) = device.create_buffer(4, usage, loc, queue);
+        let (buffer2, allocation2) = device.create_buffer(4, usage, loc, queue);
+        
+        // &mut buffer2, &buffer
+        // read buffer, write buffer2
+        recorder.copy_buffer(buffer, buffer2, vec![copy]);
+
+        // barrier should be automatically placed here
+
+        // &mut buffer, &buffer2
+        // read buffer2, write buffer
+        recorder.copy_buffer(buffer2, buffer, vec![copy]);
+        graphics.submit(recorder);
+
+        device.destroy_buffer(buffer, allocation);
+        device.destroy_buffer(buffer2, allocation2);
+    }
+
 }
 
 // Executed each frame
