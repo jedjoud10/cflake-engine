3f40737c5d9f14f0f121d4745fd46248dcabae76
pigsteel <43256029+pigsteel@users.noreply.github.com>
2023-03-24 18:48:09 -0400

    hee hee
    (noise)

--- crates/assets/src/assets/engine/shaders/noises/common.glsl
+++ crates/assets/src/assets/engine/shaders/noises/common.glsl
@@ -80,3 +80,93 @@ vec4 taylorInvSqrt(vec4 r)
 {
   return 1.79284291400159 - 0.85373472095314 * r;
 }
+
+/*
+original_author: Patricio Gonzalez Vivo
+description: pass a value and get some random normalize value between 0 and 1
+use: float random[2|3](<float|vec2|vec3> value)
+examples:
+    - /shaders/generative_random.frag
+*/
+
+float random(float x) {
+  return fract(sin(x) * 43758.5453);
+}
+
+float random(vec2 st) {
+  return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+float random(vec3 pos) {
+  return fract(sin(dot(pos.xyz, vec3(70.9898, 78.233, 32.4355))) * 43758.5453123);
+}
+
+float random(vec4 pos) {
+    float dot_product = dot(pos, vec4(12.9898,78.233,45.164,94.673));
+    return fract(sin(dot_product) * 43758.5453);
+}
+
+// Hash function from https://www.shadertoy.com/view/4djSRW
+vec3 RANDOM_SCALE3 = vec3(0.1031, 0.1030, 0.0973);
+
+vec4 RANDOM_SCALE4 = vec4(1031, 0.1030, 0.0973, 0.1099);
+
+vec2 random2(float p) {
+    vec3 p3 = fract(vec3(p) * RANDOM_SCALE3);
+    p3 += dot(p3, p3.yzx + 19.19);
+    return fract((p3.xx+p3.yz)*p3.zy);
+}
+
+vec2 random2(vec2 p) {
+    vec3 p3 = fract(p.xyx * RANDOM_SCALE3);
+    p3 += dot(p3, p3.yzx + 19.19);
+    return fract((p3.xx+p3.yz)*p3.zy);
+}
+
+vec2 random2(vec3 p3) {
+    p3 = fract(p3 * RANDOM_SCALE3);
+    p3 += dot(p3, p3.yzx+19.19);
+    return fract((p3.xx+p3.yz)*p3.zy);
+}
+
+vec3 random3(float p) {
+    vec3 p3 = fract(vec3(p) * RANDOM_SCALE3);
+    p3 += dot(p3, p3.yzx+19.19);
+    return fract((p3.xxy+p3.yzz)*p3.zyx); 
+}
+
+vec3 random3(vec2 p) {
+    vec3 p3 = fract(vec3(p.xyx) * RANDOM_SCALE3);
+    p3 += dot(p3, p3.yxz+19.19);
+    return fract((p3.xxy+p3.yzz)*p3.zyx);
+}
+
+vec3 random3(vec3 p) {
+    p = fract(p * RANDOM_SCALE3);
+    p += dot(p, p.yxz+19.19);
+    return fract((p.xxy + p.yzz)*p.zyx);
+}
+
+vec4 random4(float p) {
+    vec4 p4 = fract(vec4(p) * RANDOM_SCALE4);
+    p4 += dot(p4, p4.wzxy+19.19);
+    return fract((p4.xxyz+p4.yzzw)*p4.zywx);   
+}
+
+vec4 random4(vec2 p) {
+    vec4 p4 = fract(vec4(p.xyxy) * RANDOM_SCALE4);
+    p4 += dot(p4, p4.wzxy+19.19);
+    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
+}
+
+vec4 random4(vec3 p) {
+    vec4 p4 = fract(vec4(p.xyzx)  * RANDOM_SCALE4);
+    p4 += dot(p4, p4.wzxy+19.19);
+    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
+}
+
+vec4 random4(vec4 p4) {
+    p4 = fract(p4  * RANDOM_SCALE4);
+    p4 += dot(p4, p4.wzxy+19.19);
+    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
+}
