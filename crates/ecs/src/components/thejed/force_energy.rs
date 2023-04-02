c560979bc49311e3cd57b8e3806d93d5aa124e8c
pigsteel <43256029+pigsteel@users.noreply.github.com>
2023-04-02 12:31:19 -0400

    Update force_energy.rs

--- crates/ecs/src/components/thejed/force_energy.rs
+++ crates/ecs/src/components/thejed/force_energy.rs
@@ -19,4 +19,13 @@ impl Force {
         f32::sqrt(f32::pow(contact::x - axis_pos::x, 2) + f32::pow(contact::y - axis_pos::y, 2) + f32::pow(contact::z - axis_pos::z, 2));
         
     }
+
+    pub fn apply(&mut self, &mut velocity: Vec3, mass: f32) {
+        // Blasphemous code (real)
+        for i in forces.iter_mut() {
+            velocity += i / mass;
+        }
+
+        *forces.clear();
+    }
 }
